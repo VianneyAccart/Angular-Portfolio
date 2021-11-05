90ae040857ee96af63285599c1d479c207643cee
Vianney Accart <vianneyaccart@gmail.com>
2021-11-05 18:57:17 +0100

    creation of banner and homepage components. Add a sticky header on scroll after banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './homepage/homepage.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomepageComponent
+    HomepageComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
