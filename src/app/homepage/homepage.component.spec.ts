1bbed4d448cc2f29858546560aeca1732e3cc782
Vianney Accart <vianneyaccart@gmail.com>
2021-11-05 16:31:21 +0100

    create homepage component

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WelcomerComponent } from './welcomer.component';
+import { HomepageComponent } from './homepage.component';
 
-describe('WelcomerComponent', () => {
-  let component: WelcomerComponent;
-  let fixture: ComponentFixture<WelcomerComponent>;
+describe('HomepageComponent', () => {
+  let component: HomepageComponent;
+  let fixture: ComponentFixture<HomepageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WelcomerComponent ]
+      declarations: [ HomepageComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WelcomerComponent);
+    fixture = TestBed.createComponent(HomepageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
