4c60b1d5e5133205f78dd20963ebd5db555de3a8
Vianney Accart <vianneyaccart@gmail.com>
2021-11-06 21:28:12 +0100

    resolve displaying of projects and add some style. Need to add last projects and make images

--- src/app/realisations/realisations.component.ts
+++ src/app/realisations/realisations.component.ts
@@ -4,21 +4,41 @@ import { Project } from '../shared/models/Project.model';
 @Component({
   selector: 'app-realisations',
   templateUrl: './realisations.component.html',
-  styleUrls: ['./realisations.component.css']
+  styleUrls: ['./realisations.component.css'],
 })
 export class RealisationsComponent implements OnInit {
-
   realisations: Project[];
 
-  constructor() { 
-    this.realisations = new Array()
+  constructor() {
+    this.realisations = new Array();
   }
 
   ngOnInit(): void {
     this.realisations.push(
-      new Project("Rock'Quest", "HTML / CSS / JavaScript", "https://images.pexels.com/photos/10137509/pexels-photo-10137509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260","https://rockquest.netlify.app"),
-      new Project("Prune Manciet Ostéopathe","WordPress", "https://images.pexels.com/photos/10137509/pexels-photo-10137509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://prunemanciet-osteopathe.fr")
-    )
+      new Project(
+        "Rock'Quest",
+        'HTML / CSS / JavaScript',
+        'https://images.pexels.com/photos/10137509/pexels-photo-10137509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://rockquest.netlify.app'
+      ),
+      new Project(
+        'Prune Manciet Ostéopathe',
+        'WordPress',
+        'https://images.pexels.com/photos/10137509/pexels-photo-10137509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://prunemanciet-osteopathe.fr'
+      ),
+      new Project(
+        "Rock'Quest",
+        'HTML / CSS / JavaScript',
+        'https://images.pexels.com/photos/10137509/pexels-photo-10137509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://rockquest.netlify.app'
+      ),
+      new Project(
+        'Prune Manciet Ostéopathe',
+        'WordPress',
+        'https://images.pexels.com/photos/10137509/pexels-photo-10137509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        'https://prunemanciet-osteopathe.fr'
+      )
+    );
   }
-
 }
