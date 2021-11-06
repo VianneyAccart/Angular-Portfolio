acd9a91e9f0849df7cdefb9278fe1fe71a691ee9
Vianney Accart <vianneyaccart@gmail.com>
2021-11-06 12:46:52 +0100

    add curriculum vitae

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
+  portrait: string;
+  curriculum: string;
+  intro: string;
+
+  constructor() {
+    this.portrait = '/assets/img/portrait-vianney-accart.png';
+    this.curriculum =
+      '/assets/download/cv-vianney-accart-developpeur-web-full-stack.pdf';
+    this.intro =
+      "Mon rôle est de concevoir des solutions à la fois performantes, intuitives et responsive, qui s'adaptent à vos besoins.";
+  }
 }
