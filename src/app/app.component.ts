e836861263de0fee479130bc5466fe768905c5f4
Vianney Accart <vianneyaccart@gmail.com>
2021-11-29 19:55:18 +0100

    add SEO service and modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { CanonicalService } from './shared/services/canonical.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularPortfolio';
+export class AppComponent implements OnInit{
+  
+  constructor(private metaTagService: Meta, private canonicalService: CanonicalService){}
+
+  ngOnInit() {
+    this.metaTagService.addTags([
+      { name: 'keywords', content: 'Développeur web Lyon, Développeur Angular Lyon, Développeur Java Lyon' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Vianney Accart' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2021-11-29', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' }
+    ]);
+    this.canonicalService.setCanonicalURL();
+  }
 }
